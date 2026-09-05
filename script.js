9b227d4b9faadf20d3b99060d3cd565a2d6b5d20
SOHAMINDUSTRIES25 <gojosaturo2514090i@gmail.com>
2026-09-06 00:08:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const SUPABASE_URL =
     "https://zqxnumdpefndysbkrrcy.supabase.co";
 
 const SUPABASE_PUBLISHABLE_KEY =
-    "PASTE_YOUR_PUBLISHABLE_KEY_HERE";
+    "sb_publishable_NY-WNgp08StgIBuTj8xoCA_x2tIL-ex";
 
 const supabaseClient =
     window.supabase.createClient(
